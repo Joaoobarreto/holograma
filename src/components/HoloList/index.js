6ead4f95adf99149d37bdf656a0787b9222bb5a9
Paola Dagher <pdagher2011@hotmail.com>
2023-06-28 15:49:53 -0300

    modal-add-carrinho

--- src/components/HoloList/index.js
+++ src/components/HoloList/index.js
@@ -1,25 +1,64 @@
-import React from "react";
-import { ScrollView, View, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {ScrollView, View, Image, StyleSheet, Modal, TouchableOpacity,} from "react-native";
 import Texto from "../Texto";
 
+
+
 export default function HoloList({ items }) {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [cartMessage, setCartMessage] = useState("");
+
   const renderItems = () => {
     return items?.map((item) => (
-      <View style={estilos.item} key={item.id}>
-        <View style={estilos.image}>
+      <TouchableOpacity
+        style={estilos.item}
+        key={item.id}
+        onPress={() => setSelectedItem(item)}
+      >
+        <View style={estilos.imageContainer}>
           <Image source={item.img} style={estilos.image} />
         </View>
         <View style={estilos.itemInfo}>
           <Texto style={estilos.textBold}>{item.title}</Texto>
-          <Texto style={estilos.textBold}>{item.price}</Texto>
+          <Texto>{item.price}</Texto>
         </View>
-      </View>
+      </TouchableOpacity>
     ));
+  };
+
+
+
+  const closeModal = () => {
+    setSelectedItem(null);
+    setCartMessage("");
+  };
+
+  const addToCart = () => {
+    setCartMessage("Produto adicionado ao carrinho!");
   };
 
   return (
     <ScrollView>
       <View style={estilos.container}>{renderItems()}</View>
+
+      <Modal visible={selectedItem !== null} animationType="slide" transparent>
+        <View style={estilos.modalContainer}>
+          {selectedItem && (
+            <View style={estilos.modalContent}>
+              <TouchableOpacity style={estilos.closeButton} onPress={closeModal}>
+                <Texto style={estilos.closeButtonText}>X</Texto>
+              </TouchableOpacity>
+              <Image source={selectedItem.img} style={estilos.modalImage} />
+              <Texto style={estilos.itemName}>{selectedItem.title}</Texto>
+              <Texto style={estilos.itemPrice}>{selectedItem.price}</Texto>
+              <TouchableOpacity style={estilos.addButton} onPress={addToCart}>
+                <Texto style={estilos.buttonText}>Adicionar ao carrinho</Texto>
+              </TouchableOpacity>
+              <Texto>{cartMessage}</Texto>
+            </View>
+          )}
+        </View>
+      </Modal>
     </ScrollView>
   );
 }
@@ -32,9 +71,9 @@ const estilos = StyleSheet.create({
     padding: 10,
   },
   item: {
-    width: "40%",
+    width: "48%",
     padding: 10,
-    margin: "5%",
+    marginBottom: 10,
   },
   itemInfo: {
     marginTop: 5,
@@ -43,9 +82,62 @@ const estilos = StyleSheet.create({
   textBold: {
     fontWeight: "bold",
   },
-  image: {
+  imageContainer: {
     width: "100%",
     height: 200,
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
     resizeMode: "contain",
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalImage: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+  itemName: {
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  itemPrice: {
+    marginBottom: 20,
+  },
+  addButton: {
+    backgroundColor: "#45DD12",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+
+
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    right: 20,
+    backgroundColor: "transparent",
+    zIndex: 1,
+  },
+  closeButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000",
+  },
 });
+
