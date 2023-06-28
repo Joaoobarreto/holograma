6a2807db383ccf0a1d8362bad3ceca55ab919fb6
hendrikmelo <hendrikmelo123@gmail.com>
2023-06-28 19:09:50 -0300

    login-google

--- src/routes/profileStack.routes.js
+++ src/routes/profileStack.routes.js
@@ -3,12 +3,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfilePage from "../pages/Profile/ProfilePage";
 import InformacoesConta from "../pages/InformacoesConta";
 import MeusPedidos from "../pages/MeusPedidos";
+import Login from "../pages/Login";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
 export function ProfileStackRoutes() {
     return (
         <Navigator>
+            <Screen
+            name='Login'
+            component={Login}
+            />
             <Screen
             name='ProfilePage'
             component={ProfilePage}
